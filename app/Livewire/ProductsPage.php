41bae57b137f974df0d508615b0044db0ffbde01
lenizochristian8-afk <lenizochristian8@gmail.com>
2025-11-21 20:15:00 +0800

    15 - E-Commerce Project with Laravel 10, Livewire 3, Filament 3 & Tailwind CSS

--- app/Livewire/ProductsPage.php
+++ app/Livewire/ProductsPage.php
@@ -8,6 +8,9 @@ use App\Models\Product;
 use Livewire\WithPagination;
 use App\Models\Brand;
 use App\Models\Category;
+use Livewire\Attributes\URL;
+
+
 
 #[Title('Products - Ecommerce')]
 
@@ -15,11 +18,48 @@ class ProductsPage extends Component
 {
     use WithPagination;
 
+    #[URL]
+    public $selected_categories = [];
+
+    #[URL]
+    public $selected_brands = [];
+
+    #[URL]
+    public $featured;
+
+    #[URL]
+    public $on_sale;
+
+    #[URL]
+    public $price_range = 5000;
+
     public function render()
     {
         $productQuery = Product::query()->where('is_active', 1);
+
+         if(!empty($this->selected_categories)) {
+            $productQuery->whereIn('category_id', $this->selected_categories);
+        }
+
+        if(!empty($this->selected_brands)) {
+            $productQuery->whereIn('brand_id', $this->selected_brands);
+        }
+
+        if($this->featured) {
+            $productQuery->where('is_featured', 1);
+        }
+
+        if($this->on_sale) {
+            $productQuery->where('on_sale', 1);
+        }
+
+        if($this->price_range) {
+            $productQuery->whereBetween('price', [0, $this->price_range]);
+        }
+
+        
         return view('livewire.products-page', [
-            'products' => $productQuery->paginate(6),
+            'products' => $productQuery->paginate(9),
             'brands' => Brand::where('is_active', 1)->get(['id', 'name', 'slug']),
             'categories' => Category::where('is_active', 1)->get(['id', 'name', 'slug']),
         ]);
