69c1659b341acd0452f479df5e41cf713144992f
lenizochristian8-afk <lenizochristian8@gmail.com>
2025-11-21 13:11:37 +0800

    13 - E-Commerce Project with Laravel 10, Livewire 3, Filament 3 & Tailwind CSS

--- app/Livewire/CategoriesPage.php
+++ app/Livewire/CategoriesPage.php
@@ -3,11 +3,18 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Livewire\Attributes\Title;
+use App\Models\Category;
+
+#[Title('Categories Page - Ecommerce')]
 
 class CategoriesPage extends Component
 {
     public function render()
     {
-        return view('livewire.categories-page');
+        $categories = Category::where('is_active', 1)->get();
+        return view('livewire.categories-page', [
+            'categories' => $categories,
+        ]);
     }
 }
--- app/Livewire/HomePage.php
+++ app/Livewire/HomePage.php
@@ -3,12 +3,22 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Livewire\Attributes\Title;
+use App\Models\Brand;
+use App\Models\Category;
+
+#[Title('Home Page - Ecommerce')]
 
 class HomePage extends Component
 {
     public function render()
     {
-        return view('livewire.home-page');
+        $brands = Brand::where('is_active', 1)->get();
+        $categories = Category::where('is_active', 1)->get();
+        return view('livewire.home-page', [
+            'brands' => $brands,
+            'categories' => $categories,
+        ]);
     }
 }
 
