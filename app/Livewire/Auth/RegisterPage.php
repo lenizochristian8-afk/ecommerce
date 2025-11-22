68af2a834a06a0cf3a5278250dc5bc5885d4e344
lenizochristian8-afk <lenizochristian8@gmail.com>
2025-11-22 15:09:44 +0800

    19 - E-Commerce Project with Laravel 10, Livewire 3, Filament 3 & Tailwind CSS

--- app/Livewire/Auth/RegisterPage.php
+++ app/Livewire/Auth/RegisterPage.php
@@ -3,9 +3,43 @@
 namespace App\Livewire\Auth;
 
 use Livewire\Component;
+use Livewire\Attributes\Title;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
+
+#[Title('Register')]
 class RegisterPage extends Component
 {
+
+    public $name;
+    public $email;
+    public $password;
+
+    //register user
+    public function save(){
+      $this->validate([
+        'name' => 'required|max:255',
+        'email' => 'required|email|unique:users|max:255',
+        'password' => 'required|min:6|max:255',
+      ]);
+
+      //save to database
+      $user = User::create([
+        'name' => $this->name,
+        'email' => $this->email,
+        'password' => Hash::make($this->password),
+      ]);
+
+      //login user
+      auth()->login($user);
+
+      //Redirect to homepage
+      return redirect()->intended();
+    }
+
+
+
     public function render()
     {
         return view('livewire.auth.register-page');
