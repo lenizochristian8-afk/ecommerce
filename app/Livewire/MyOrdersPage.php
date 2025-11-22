88a3172b12e8014dca230f16295454caecd00bac
lenizochristian8-afk <lenizochristian8@gmail.com>
2025-11-22 21:24:33 +0800

    24 - E-Commerce Project with Laravel 10, Livewire 3, Filament 3 & Tailwind CSS

--- app/Livewire/MyOrdersPage.php
+++ app/Livewire/MyOrdersPage.php
@@ -3,11 +3,20 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Livewire\Attributes\Title;
+use App\Models\Order;
+use Livewire\WithPagination;
 
+#[Title('My Orders - Ecommerce')]
 class MyOrdersPage extends Component
 {
+    use WithPagination;
     public function render()
     {
-        return view('livewire.my-orders-page');
+        $my_orders = Order::where('user_id', auth()->id())->latest()->paginate(5);
+        return view('livewire.my-orders-page', [
+            'orders' => $my_orders,
+
+        ]);
     }
 }
